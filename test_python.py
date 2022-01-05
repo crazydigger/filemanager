e3caa3beeeb1ecbacf733d898ee7193e42a9adac
borovirp <40pobGsb>
2022-01-05 10:51:52 +0300

    new update

---
+++ test_python.py
@@ -0,0 +1,13 @@
+from math import pi, pow, hypot, sqrt
+
+def test_pi():
+    assert pi==3.141592653589793
+def test_pow():
+    assert pow(2,0)==1
+    assert pow (2,3 ==8)
+def test_hypot():
+    assert hypot(1,1)==1
+def test_sqrt():
+    assert sqrt(9)==3
+
+
